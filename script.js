5259658f8cb4cf48759a3e4942c8e7be14e36529
Jaycen <jaycen9887@gmail.com>
2017-06-29 19:37:08 -0400

    updating js

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA9FQeqDhp1XtMJ445FD9Zw11cehnDoGIo",
+    authDomain: "employeedirectory-d2bc2.firebaseapp.com",
+    databaseURL: "https://employeedirectory-d2bc2.firebaseio.com",
+    projectId: "employeedirectory-d2bc2",
+    storageBucket: "",
+    messagingSenderId: "657337647452"
+  };
+  firebase.initializeApp(config);
+
 //add rows to output table
 $("#submit").click(function(){
     
     //variables
-    var date = new Date().;
-    
+    var month = new Date().getMonth;
+    var day = new Date().getDay;
+    var year = new Date().getYear;
+    var date = day + "/" + month + "/" + year;
+
     var row = $("<tr>");
-    var column = $("<td>");
     
     var name = $("#employee").val().trim();
     var role = $("#role").val().trim();
-    //var email = $("#email").val().trim();
     var startDate = $("#startDate").val().trim();
     var monthsWorked = date - startDate;
     var monthlyRate = $("#monthlyRate").val().trim();
@@ -17,7 +29,6 @@ $("#submit").click(function(){
     
     row.append("<td>" + name +"</td>");
     row.append("<td>" + role +"</td>");
-    //var row.append("<td>" + email +"</td>");
     row.append("<td>" + startDate +"</td>");
     row.append("<td>" + monthsWorked +"</td");
     row.append("<td>" + monthlyRate +"</td>");
