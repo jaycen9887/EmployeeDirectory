04ed11e9f75a8b29800f383bece0a83a5561cb85
Jaycen <jaycen9887@gmail.com>
2017-06-29 19:23:44 -0400

    js update

--- script.js
+++ script.js
@@ -2,10 +2,23 @@
 $("#submit").click(function(){
     
     //variables
+    var date = new Date();
     var row = $("<tr>");
     var column = $("<td>");
     
+    var name = $("#employee").val().trim();
+    var role = $("#role").val().trim();
+    //var email = $("#email").val().trim();
+    var startDate = $("#startDate").val().trim();
+    var monthsWorked = date - startDate;
+    var monthlyRate = $("#monthlyRate").val().trim();
+    var totalBill = monthsWorked * monthlyRate;
+    
+    
+
+    
     $(".table").append(row);
-    //enmp
+    
+    
     
 });
