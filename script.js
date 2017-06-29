097c801308209d8251998d06d2dec5f543c9d192
Jaycen <jaycen9887@gmail.com>
2017-06-29 19:05:36 -0400

    adding js

---
+++ script.js
@@ -0,0 +1,11 @@
+//add rows to output table
+$("#submit").click(function(){
+    
+    //variables
+    var row = $("<tr>");
+    var column = $("<td>");
+    
+    $(".table").append(row);
+    //enmp
+    
+});
