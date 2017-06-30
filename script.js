2cc78eb97b7bae9b71b97f2d9a5c5ef271d5714e
Kurt Griarte <kurtgee11@gmail.com>
2017-06-29 20:27:35 -0400

    js update

--- script.js
+++ script.js
@@ -7,15 +7,15 @@ var monthlyRate;
 var totalBill;
 
 // Initialize Firebase
-  var config = {
+var config = {
     apiKey: "AIzaSyA9FQeqDhp1XtMJ445FD9Zw11cehnDoGIo",
     authDomain: "employeedirectory-d2bc2.firebaseapp.com",
     databaseURL: "https://employeedirectory-d2bc2.firebaseio.com",
     projectId: "employeedirectory-d2bc2",
     storageBucket: "",
     messagingSenderId: "657337647452"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
 var database = firebase.database();
 
@@ -33,9 +33,9 @@ var database = firebase.database();
 
 
 //add rows to output table
-$("#submit").click(function(e){
+$("#submit").click(function (e) {
     e.preventDefault();
-    
+
     //variables
     var month = new Date().getMonth;
     var day = new Date().getDay;
@@ -43,25 +43,25 @@ $("#submit").click(function(e){
     var date = day + "/" + month + "/" + year;
 
     var row = $("<tr>");
-    
+
     name = $("#employee").val().trim();
     role = $("#role").val().trim();
     startDate = $("#startDate").val().trim();
     monthsWorked = date - startDate;
     monthlyRate = $("#monthlyRate").val().trim();
     totalBill = (monthsWorked * monthlyRate);
-    
-    row.append("<td>" + name +"</td>");
-    row.append("<td>" + role +"</td>");
-    row.append("<td>" + startDate +"</td>");
-    row.append("<td>" + 33 +"</td");
-    row.append("<td>" + monthlyRate +"</td>");
-    row.append("<td>" + (33 * 2200) +"</td>");
-    
+
+    row.append("<td>" + name + "</td>");
+    row.append("<td>" + role + "</td>");
+    row.append("<td>" + startDate + "</td>");
+    row.append("<td>" + 33 + "</td");
+    row.append("<td>" + monthlyRate + "</td>");
+    row.append("<td>" + (33 * 2200) + "</td>");
+
     $(".table").append(row);
-    
-    
-    
+
+
+
     database.ref("employes").push({
         name: name,
         role: role,
@@ -70,7 +70,7 @@ $("#submit").click(function(e){
         montlyRate: monthlyRate,
         totalBill: totalBill
     });
-    
+
 });
 
 
@@ -78,9 +78,7 @@ $("#submit").click(function(e){
 
 
 
-database.ref('employees/').on("child_added", function(childSnapshot){
-    
-   
+database.ref('employees/').on("child_added", function (childSnapshot) {
+
+
 });
-
-
